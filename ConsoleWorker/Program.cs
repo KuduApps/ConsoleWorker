aa13237893cdaf1e3eba15ccb8de20113a31d4e8
Amit Apple <amitap@microsoft.com>
2013-10-04 10:51:58 -0700

    Console worker test project

---
+++ ConsoleWorker/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleWorker
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            File.WriteAllText("verification.txt", "Verified!!!");
+            Console.WriteLine("Verification file written");
+        }
+    }
+}
