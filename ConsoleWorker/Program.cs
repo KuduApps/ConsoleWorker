5efd2665c67da7e0787bbf7fd302f8e1e720ec7e
Amit Apple <amitap@microsoft.com>
2013-12-31 15:00:33 -0800

    Added app.config

--- ConsoleWorker/Program.cs
+++ ConsoleWorker/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Configuration;
 using System.IO;
 using System.Text;
 using System.Threading;
@@ -9,8 +10,11 @@ namespace ConsoleWorker
     {
         private static void Main(string[] args)
         {
-            string path = Environment.GetEnvironmentVariable("WEBROOT_PATH") + "\\..\\..\\LogFiles\\verification.txt";
-            using (var fs = File.Open(path, FileMode.Append, FileAccess.Write, FileShare.Read | FileShare.Delete))
+            string appSettingsFilePath = Environment.GetEnvironmentVariable("WEBROOT_PATH") + "\\..\\..\\LogFiles\\appSettings.txt";
+            File.WriteAllText(appSettingsFilePath, GetFileContent());
+
+            string verificationFilePath = Environment.GetEnvironmentVariable("WEBROOT_PATH") + "\\..\\..\\LogFiles\\verification.txt";
+            using (var fs = File.Open(verificationFilePath, FileMode.Append, FileAccess.Write, FileShare.Read | FileShare.Delete))
             {
                 byte[] bytes = Encoding.UTF8.GetBytes("Verified!!!" + Environment.NewLine);
                 fs.Write(bytes, 0, bytes.Length);
@@ -24,6 +28,22 @@ namespace ConsoleWorker
                     Thread.Sleep(1000);
                 }
             }
+        }
+
+        private static string GetFileContent()
+        {
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine(ConfigurationManager.AppSettings["testSetting1"]);
+            stringBuilder.AppendLine(ConfigurationManager.AppSettings["testSetting2"]);
+            stringBuilder.AppendLine(ConfigurationManager.AppSettings["testSettingExtra"]);
+
+            foreach (ConnectionStringSettings c in ConfigurationManager.ConnectionStrings)
+            {
+                stringBuilder.AppendLine(c.Name + " " + c.ConnectionString + " " + c.ProviderName);
+            }
+
+            return stringBuilder.ToString();
         }
     }
 }
