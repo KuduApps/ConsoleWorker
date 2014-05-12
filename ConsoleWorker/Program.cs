b21f597e66e349e2bcfe70ea233e4e098ccb1ff6
amitapl <amitapl@outlook.com>
2014-05-12 11:27:33 -0700

    Make webjobs instance aware.

--- ConsoleWorker/Program.cs
+++ ConsoleWorker/Program.cs
@@ -10,10 +10,11 @@ namespace ConsoleWorker
     {
         private static void Main(string[] args)
         {
-            string appSettingsFilePath = Environment.GetEnvironmentVariable("WEBROOT_PATH") + "\\..\\..\\LogFiles\\appSettings.txt";
+            string instanceId = GetInstanceId();
+            string appSettingsFilePath = Environment.GetEnvironmentVariable("WEBROOT_PATH") + "\\..\\..\\LogFiles\\appSettings.txt." + instanceId;
             File.WriteAllText(appSettingsFilePath, GetFileContent());
 
-            string verificationFilePath = Environment.GetEnvironmentVariable("WEBROOT_PATH") + "\\..\\..\\LogFiles\\verification.txt";
+            string verificationFilePath = Environment.GetEnvironmentVariable("WEBROOT_PATH") + "\\..\\..\\LogFiles\\verification.txt." + instanceId;
             using (var fs = File.Open(verificationFilePath, FileMode.Append, FileAccess.Write, FileShare.Read | FileShare.Delete))
             {
                 byte[] bytes = Encoding.UTF8.GetBytes("Verified!!!" + Environment.NewLine);
@@ -28,6 +29,12 @@ namespace ConsoleWorker
                     Thread.Sleep(1000);
                 }
             }
+        }
+
+        private static string GetInstanceId()
+        {
+            string instanceId = Environment.GetEnvironmentVariable("WEBSITE_INSTANCE_ID") ?? Environment.MachineName;
+            return instanceId.Substring(0, instanceId.Length > 6 ? 6 : instanceId.Length);
         }
 
         private static string GetFileContent()
